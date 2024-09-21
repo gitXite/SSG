306bf0f4482f10acf1086cacffcce7e87b1768f8
gitXite <daniel.halaas@gmail.com>
2024-09-21 17:46:00 +0200

    Added more test cases

--- src/test_textnode.py
+++ src/test_textnode.py
@@ -9,5 +9,29 @@ class TestTextNode(unittest.TestCase):
         self.assertEqual(node, node2)
 
 
+    def test_url_none(self):
+        node = TextNode("This is also a test node", "italic", None)
+        node2 = TextNode("This is also a test node", "italic", None)
+        self.assertEqual(node, node2)
+
+
+    def test_noneq(self):
+        node = TextNode("We are not the same", "bold")
+        node2 = TextNode("Are you sure?", "bold")
+        self.assertNotEquals(node, node2)
+
+    
+    def test_noneq2(self):
+        node = TextNode("We are the same", "Italic")
+        node2 = TextNode("We are the same", "italic")
+        self.assertNotEquals(node, node2)
+
+
+    def test_url(self):
+        node = TextNode("Text", "bold", "https://www.text.com")
+        node2 = TextNode("Text", "bold", "https://www.text.com")
+        self.assertEqual(node, node2)
+
+
 if __name__ == "__main__":
     unittest.main()
