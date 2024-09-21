2724bc9abe9ac8a98287845d7e3ab0c7ae00a624
Daniel <Daniel.halaas@gmail.com>
2024-09-21 11:34:04 +0200

    Update main.py

--- src/main.py
+++ src/main.py
@@ -3,7 +3,7 @@ from textnode import TextNode
 
 def main():
 
-    textnode = TextNode("Text", "bold")
+    textnode = TextNode("Text", "bold", "https://www.text.com")
     print(textnode)
 
 if __name__ == "__main__":
