770f5e47448b2de8696f314fe11e66f93f9e220e
gitXite <daniel.halaas@gmail.com>
2024-09-22 13:22:17 +0200

    Tested the repr method in htmlnode

--- src/main.py
+++ src/main.py
@@ -1,10 +1,14 @@
 from textnode import TextNode
+from htmlnode import HTMLNode
 
 
 def main():
 
     textnode = TextNode("Text", "bold", "https://www.text.com")
     print(textnode)
+
+    htmlnode = HTMLNode("p", "Hello world", None, None)
+    print(htmlnode)
 
 if __name__ == "__main__":
     main()
