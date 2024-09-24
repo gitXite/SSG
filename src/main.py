df8c0dd6822c9b2c15371bc6f5ac03854993b3ef
gitXite <daniel.halaas@gmail.com>
2024-09-24 21:36:43 +0200

    Added debugging statements in main

--- src/main.py
+++ src/main.py
@@ -1,9 +1,15 @@
 from textnode import TextNode
-from htmlnode import HTMLNode, LeafNode
+from htmlnode import HTMLNode, LeafNode, ParentNode
 
 def main():
 
-    pass
+    leafnodes = [
+        LeafNode("i", "italic text"), 
+        LeafNode("b", "bold text"),
+        LeafNode(None, "Raw text"),
+        ], 
+    parentnode = ParentNode("p", None, leafnodes) # this doesnt really need None for value argument, needs to be fixed
+    print(parentnode.to_html())
 
 if __name__ == "__main__":
     main()
