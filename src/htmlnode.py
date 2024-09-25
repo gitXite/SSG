c667739dfb9d3aaa6d36bf6c36684b2883f624f7
Daniel <Daniel.halaas@gmail.com>
2024-09-25 18:16:14 +0200

    Error checks first

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -25,12 +25,12 @@ class LeafNode(HTMLNode):
         super().__init__(tag, value, None, props)
     
     def to_html(self):
-        props_html = self.props_to_html()
-
         if self.value is None or self.value == "":
             raise ValueError("all leaf nodes must have a value")
         elif self.tag is None or self.tag == "":
             return self.value
+            
+        props_html = self.props_to_html()
         
         if self.props is None or len(self.props) == 0:
                 return f"<{self.tag}>{self.value}</{self.tag}>"
@@ -45,15 +45,15 @@ class ParentNode(HTMLNode):
         super().__init__(tag, None, children, props)
 
     def to_html(self):
-        result = ""
-        props_html = self.props_to_html()
-        
         if self.tag is None or self.tag == "":
             raise ValueError("all parent nodes must have a tag")
         elif self.children is None or len(self.children) == 0:
             raise ValueError("missing child node")
         elif type(self.children) != list:
             raise Exception("child nodes must be contained in a list")
+        
+        result = ""
+        props_html = self.props_to_html()
         
         for node in self.children:
             result += node.to_html()
