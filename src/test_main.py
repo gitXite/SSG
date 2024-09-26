5d15d2bd3168a0f921868ed6dfd442577d1eeec0
Daniel <Daniel.halaas@gmail.com>
2024-09-26 09:49:17 +0200

    Format

--- src/test_main.py
+++ src/test_main.py
@@ -1,8 +1,8 @@
 import unittest
 from main import *
+from node_helpers import *
 from textnode import TextNode
 from htmlnode import HTMLNode, LeafNode, ParentNode
-from node_helpers import *
 
 class TestTextNodeToLeafNodeConversion(unittest.TestCase):
   pass
