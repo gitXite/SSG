ae58b74b3014b72b2d5e75dddc522a05506ce3f5
gitXite <daniel.halaas@gmail.com>
2024-09-30 22:28:15 +0200

    Started testing split_nodes_delimiter

--- src/test_textnode.py
+++ src/test_textnode.py
@@ -41,42 +41,54 @@ class TestTextNode(unittest.TestCase):
         self.assertEqual(repr(node), "TextNode(Printed, text, https://www.url.com)")
 
 class TestTextNodeToHTMLNode(unittest.TestCase):
-	def test_text_node(self):
-		node = TextNode("Test", "text")
-		self.assertEqual(create_text_node(node), LeafNode(None, "Test"))
-		
-	def test_bold_node(self):
-		node = TextNode("Test", "bold")
-		self.assertEqual(create_bold_node(node), LeafNode("b", "Test"))
+    def test_text_node(self):
+        node = TextNode("Test", "text")
+        self.assertEqual(create_text_node(node), LeafNode(None, "Test"))
 
-	def test_italic_node(self):
-		node = TextNode("Test", "italic")
-		self.assertEqual(create_italic_node(node), LeafNode("i", "Test"))
+    def test_bold_node(self):
+        node = TextNode("Test", "bold")
+        self.assertEqual(create_bold_node(node), LeafNode("b", "Test"))
 
-	def test_code_node(self):
-		node = TextNode("Test", "code")
-		self.assertEqual(create_code_node(node), LeafNode("code", "Test"))
+    def test_italic_node(self):
+        node = TextNode("Test", "italic")
+        self.assertEqual(create_italic_node(node), LeafNode("i", "Test"))
 
-	def test_link_node(self):
-		node = TextNode("Click me!", "link", "https://www.google.com")
-		self.assertEqual(create_link_node(node), LeafNode("a", "Click me!", {"href": "https://www.google.com"}))
+    def test_code_node(self):
+        node = TextNode("Test", "code")
+        self.assertEqual(create_code_node(node), LeafNode("code", "Test"))
 
-	def test_img_node(self):
-		node = TextNode("Google logo", "img", "image.jpg")
-		self.assertEqual(create_img_node(node), LeafNode("img", "", {"src": "image.jpg", "alt": "Google logo"}))
+    def test_link_node(self):
+        node = TextNode("Click me!", "link", "https://www.google.com")
+        self.assertEqual(create_link_node(node), LeafNode("a", "Click me!", {"href": "https://www.google.com"}))
 
-	def test_text_node_to_html_node_invalid_type(self):
-		text_node = TextNode("Test", "invalid")
-		with self.assertRaises(Exception):
-			text_node_to_html_node(text_node)
+    def test_img_node(self):
+        node = TextNode("Google logo", "img", "image.jpg")
+        self.assertEqual(create_img_node(node), LeafNode("img", "", {"src": "image.jpg", "alt": "Google logo"}))
 
-	def test_text_node_to_html_node(self):
-		text_node = TextNode("Test", "italic")
-		self.assertEqual(text_node_to_html_node(text_node), LeafNode("i", "Test"))
+    def test_text_node_to_html_node_invalid_type(self):
+        text_node = TextNode("Test", "invalid")
+        with self.assertRaises(Exception):
+            text_node_to_html_node(text_node)
+
+    def test_text_node_to_html_node(self):
+        text_node = TextNode("Test", "italic")
+        self.assertEqual(text_node_to_html_node(text_node), LeafNode("i", "Test"))
+
+old_nodes = [
+	TextNode("This is text with a `code block` word", "text"),
+	TextNode("This is just some raw text", "text"),
+	TextNode("This is text with **bold** delimiters", "text")
+]
 
 class TestSplitNodesDelimiter(unittest.TestCase):
-	def test_type_list(self):
-		pass
+    def test_type_list(self):
+        node = TextNode("This is text with a `code block` word", "text")
+        with self.assertRaises(TypeError):
+            split_nodes_delimiter(node, "`", "code")
+			
+    def test_empty_node(self):
+        with self.assertRaises(ValueError):
+            split_nodes_delimiter([], "*", "italic")
 
 if __name__ == "__main__":
     unittest.main()
