af14a5b3ef7aa0c230ac2c2739a7268bbaa2a725
Daniel <Daniel.halaas@gmail.com>
2024-09-30 20:03:06 +0200

    Changed function location from main to textnode

--- src/main.py
+++ src/main.py
@@ -1,12 +1,5 @@
 from textnode import TextNode
 from htmlnode import HTMLNode, LeafNode, ParentNode
-from node_helpers import *
-
-# conversion function using dictionary dispatch pattern from node_helpers.py
-def text_node_to_html_node(text_node):
-    if text_node.text_type not in type_to_node:
-        raise Exception(f"invalid text type: {text_node.text_type}")
-    return type_to_node[text_node.text_type](text_node)
 
 def main():
 
