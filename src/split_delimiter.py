303808cbda94fd95352679450198282557badcf7
gitXite <daniel.halaas@gmail.com>
2024-09-30 12:22:23 +0200

    Started on split_delimiter

--- src/split_delimiter.py
+++ src/split_delimiter.py
@@ -1,4 +1,15 @@
 from textnode import TextNode
 
 def split_nodes_delimiter(old_nodes: list, delimiter: str, text_type: str):
-    pass
+    # error catches
+    if type(old_nodes) != list:
+        raise TypeError("nodes must be contained in a list")
+    if len(old_nodes) == 0:
+        raise ValueError("not a node")
+    
+    new_nodes = []
+
+    for node in old_nodes:
+        if text_type == "text" and delimiter in node.text:
+            new_nodes.append(node.split(delimiter)) # probably wrong
+    print(new_nodes)
