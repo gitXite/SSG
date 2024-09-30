d96f913b1f98a74b21f45ad552be487abd10dcb5
Daniel <Daniel.halaas@gmail.com>
2024-09-30 20:05:25 +0200

    Changed function location from main to textnode

--- src/test_textnode.py
+++ src/test_textnode.py
@@ -1,5 +1,6 @@
 import unittest
 from textnode import TextNode
+from node_helpers import *
 
 class TestTextNode(unittest.TestCase):
     def test_eq(self):
@@ -35,6 +36,40 @@ class TestTextNode(unittest.TestCase):
     def test_repr(self):
         node = TextNode("Printed", "text", "https://www.url.com")
         self.assertEqual(repr(node), "TextNode(Printed, text, https://www.url.com)")
+
+class TestTextNodeToLeafNodeConversion(unittest.TestCase):
+	def test_text_node(self):
+		node = TextNode("Test", "text")
+		self.assertEqual(create_text_node(node), LeafNode(None, "Test"))
+		
+	def test_bold_node(self):
+		node = TextNode("Test", "bold")
+		self.assertEqual(create_bold_node(node), LeafNode("b", "Test"))
+
+	def test_italic_node(self):
+		node = TextNode("Test", "italic")
+		self.assertEqual(create_italic_node(node), LeafNode("i", "Test"))
+
+	def test_code_node(self):
+		node = TextNode("Test", "code")
+		self.assertEqual(create_code_node(node), LeafNode("code", "Test"))
+
+	def test_link_node(self):
+		node = TextNode("Click me!", "link", "https://www.google.com")
+		self.assertEqual(create_link_node(node), LeafNode("a", "Click me!", {"href": "https://www.google.com"}))
+
+	def test_img_node(self):
+		node = TextNode("Google logo", "img", "image.jpg")
+		self.assertEqual(create_img_node(node), LeafNode("img", "", {"src": "image.jpg", "alt": "Google logo"}))
+
+	def test_text_node_to_html_node_invalid_type(self):
+		text_node = TextNode("Test", "invalid")
+		with self.assertRaises(Exception):
+			text_node_to_html_node(text_node)
+
+	def test_text_node_to_html_node(self):
+		text_node = TextNode("Test", "italic")
+		self.assertEqual(text_node_to_html_node(text_node), LeafNode("i", "Test"))
 
 if __name__ == "__main__":
     unittest.main()
