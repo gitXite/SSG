8b464bb290fcfc7ebf0296158c23ff45a965c8a0
Daniel <Daniel.halaas@gmail.com>
2024-10-02 14:31:39 +0200

    More test cases

--- src/test_textnode.py
+++ src/test_textnode.py
@@ -172,6 +172,17 @@ class TestExtractMarkdown(unittest.TestCase):
             ("rick roll", "https://i.imgur.com/aKaOqIh.gif"), 
             ("obi wan", "https://i.imgur.com/fJRm4Vk.jpeg")
         ])
+
+    def test_extract_markdown_images_without_alt_text(self):
+        text = "This is text with a ![](https://i.imgur.com/aKaOqIh.gif) and ![](https://i.imgur.com/fJRm4Vk.jpeg)"
+        self.assertEqual(extract_markdown_images(text), [
+            ("", "https://i.imgur.com/aKaOqIh.gif"), 
+            ("", "https://i.imgur.com/fJRm4Vk.jpeg")
+        ])
+
+    def test_extract_markdown_images_no_matches(self):
+        text = "This is text with a [](https://i.imgur.com/aKaOqIh.gif) and !https://i.imgur.com/fJRm4Vk.jpeg"
+        self.assertEqual(extract_markdown_images(text), [])
         
     def test_extract_markdown_links(self):
         text = "This is text with a link [to boot dev](https://www.boot.dev) and [to youtube](https://www.youtube.com/@bootdotdev)"
@@ -179,6 +190,17 @@ class TestExtractMarkdown(unittest.TestCase):
             ("to boot dev", "https://www.boot.dev"), 
             ("to youtube", "https://www.youtube.com/@bootdotdev")
         ])
+
+    def test_extract_markdown_links_without_link(self):
+        text = "This is text with a link [](https://www.boot.dev) and [](https://www.youtube.com/@bootdotdev)"
+        self.assertEqual(extract_markdown_links(text), [
+            ("", "https://www.boot.dev"), 
+            ("", "https://www.youtube.com/@bootdotdev")
+        ])
+
+    def test_extract_markdown_links_no_matches(self):
+        text = "This is text with a link to boot devhttps://www.boot.dev and to youtubehttps://www.youtube.com/@bootdotdev"
+        self.assertEqual(extract_markdown_links(text), [])
         
 if __name__ == "__main__":
     unittest.main()
