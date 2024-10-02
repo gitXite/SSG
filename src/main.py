7592586852566806a8b40cead7e1cf5b8a7ce7ef
gitXite <daniel.halaas@gmail.com>
2024-10-02 12:20:46 +0200

    Print tests to see if extracting markdown works as expected

--- src/main.py
+++ src/main.py
@@ -1,4 +1,4 @@
-from textnode import TextNode
+from textnode import TextNode, extract_markdown_images, extract_markdown_links
 from htmlnode import HTMLNode, LeafNode, ParentNode
 from split_delimiter import split_nodes_delimiter
 
@@ -26,6 +26,12 @@ def main():
     ]
     print(split_nodes_delimiter(old_nodes, "`", "code"))
     """
+
+    # testing extracting images from markdown
+    text = "This is text with a ![rick roll](https://i.imgur.com/aKaOqIh.gif) and ![obi wan](https://i.imgur.com/fJRm4Vk.jpeg)"
+    print(extract_markdown_images(text))
+    text2 = "This is text with a link [to boot dev](https://www.boot.dev) and [to youtube](https://www.youtube.com/@bootdotdev)"
+    print(extract_markdown_links(text2))
 
 if __name__ == "__main__":
     main()
