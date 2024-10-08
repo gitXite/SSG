a7aae0bfa05f233b4e8234ffc0be9553b38caa7f
Daniel <Daniel.halaas@gmail.com>
2024-10-08 17:53:50 +0200

    Continue with markdown_to_html_node

--- src/markdown_functions.py
+++ src/markdown_functions.py
@@ -53,5 +53,7 @@ def block_to_block_type(block):
 
 # converts full markdown documents to html nodes
 def markdown_to_html_node(markdown):
-    pass
+    blocks = markdown_to_blocks(markdown)
+    for block in blocks:
+        block_type = block_to_block_type(block)
     
