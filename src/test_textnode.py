45e4a80a3a03bfd58d602a04f4505277a4c49dfa
Daniel <Daniel.halaas@gmail.com>
2024-10-08 16:10:33 +0200

    Moved markdown extraction functions to separate file

--- src/test_textnode.py
+++ src/test_textnode.py
@@ -73,43 +73,6 @@ class TestTextNodeToHTMLNode(unittest.TestCase):
         text_node = TextNode("Test", "italic")
         self.assertEqual(text_node_to_html_node(text_node), LeafNode("i", "Test"))
 
-class TestExtractMarkdown(unittest.TestCase):
-    def test_extract_markdown_images(self):
-        text = "This is text with a ![rick roll](https://i.imgur.com/aKaOqIh.gif) and ![obi wan](https://i.imgur.com/fJRm4Vk.jpeg)"
-        self.assertEqual(extract_markdown_images(text), [
-            ("rick roll", "https://i.imgur.com/aKaOqIh.gif"), 
-            ("obi wan", "https://i.imgur.com/fJRm4Vk.jpeg")
-        ])
-
-    def test_extract_markdown_images_without_alt_text(self):
-        text = "This is text with a ![](https://i.imgur.com/aKaOqIh.gif) and ![](https://i.imgur.com/fJRm4Vk.jpeg)"
-        self.assertEqual(extract_markdown_images(text), [
-            ("", "https://i.imgur.com/aKaOqIh.gif"), 
-            ("", "https://i.imgur.com/fJRm4Vk.jpeg")
-        ])
-
-    def test_extract_markdown_images_no_matches(self):
-        text = "This is text with a [](https://i.imgur.com/aKaOqIh.gif) and !https://i.imgur.com/fJRm4Vk.jpeg"
-        self.assertEqual(extract_markdown_images(text), [])
-        
-    def test_extract_markdown_links(self):
-        text = "This is text with a link [to boot dev](https://www.boot.dev) and [to youtube](https://www.youtube.com/@bootdotdev)"
-        self.assertEqual(extract_markdown_links(text), [
-            ("to boot dev", "https://www.boot.dev"), 
-            ("to youtube", "https://www.youtube.com/@bootdotdev")
-        ])
-
-    def test_extract_markdown_links_without_link(self):
-        text = "This is text with a link [](https://www.boot.dev) and [](https://www.youtube.com/@bootdotdev)"
-        self.assertEqual(extract_markdown_links(text), [
-            ("", "https://www.boot.dev"), 
-            ("", "https://www.youtube.com/@bootdotdev")
-        ])
-
-    def test_extract_markdown_links_no_matches(self):
-        text = "This is text with a link to boot devhttps://www.boot.dev and to youtubehttps://www.youtube.com/@bootdotdev"
-        self.assertEqual(extract_markdown_links(text), [])
-
 class TestTextToTextnodes(unittest.TestCase):
     def test_text_to_textnodes(self):
         pass
