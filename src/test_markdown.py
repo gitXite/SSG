17c609aa0e3b033aa4bae01ba91fa8ca4836cd44
Daniel <Daniel.halaas@gmail.com>
2024-10-14 15:24:17 +0200

    Moved tests from split_functions to markdown

--- src/test_markdown.py
+++ src/test_markdown.py
@@ -1,6 +1,93 @@
 import unittest
 from markdown_functions import *
 
+old_nodes = [
+	TextNode("This is text with a `code block` word", "text"),
+	TextNode("This is just some raw text", "text"),
+	TextNode("This is text with **bold** delimiters", "text")
+]
+
+class TestSplitNodesDelimiter(unittest.TestCase):
+    def test_type_list(self):
+        node = TextNode("This is text with a `code block` word", "text")
+        with self.assertRaises(TypeError):
+            split_nodes_delimiter(node, "`", "code")
+			
+    def test_empty_node(self):
+        self.assertEqual(split_nodes_delimiter([], "**", "bold"), [])
+
+    def test_split_nodes_delimiter(self):
+        self.assertEqual(split_nodes_delimiter(old_nodes, "`", "code"), [
+            TextNode("This is text with a ", "text"),
+            TextNode("code block", "code"),
+            TextNode(" word", "text"),
+            TextNode("This is just some raw text", "text"),
+            TextNode("This is text with **bold** delimiters", "text")
+        ])
+
+    def test_split_nodes_delimiter2(self):
+        self.assertEqual(split_nodes_delimiter(old_nodes, "**", "bold"), [
+            TextNode("This is text with a `code block` word", "text"),
+            TextNode("This is just some raw text", "text"),
+            TextNode("This is text with ", "text"),
+            TextNode("bold", "bold"),
+            TextNode(" delimiters", "text")
+        ])
+
+    def test_split_nodes_delimiter_multiple_delimiters(self):
+        node = TextNode("This is some *text* with *multiple* delimiters", "text")
+        self.assertEqual(split_nodes_delimiter([node], "*", "italic"), [
+            TextNode("This is some ", "text"),
+            TextNode("text", "italic"),
+            TextNode(" with ", "text"),
+            TextNode("multiple", "italic"),
+            TextNode(" delimiters", "text")
+        ])
+
+    def test_split_nodes_delimiter_startswith(self):
+        node = TextNode("**This is text** with a delimiter at the start", "text")
+        self.assertEqual(split_nodes_delimiter([node], "**", "bold"), [
+            TextNode("This is text", "bold"),
+            TextNode(" with a delimiter at the start", "text")
+        ])
+
+    def test_split_nodes_delimiter_endswith(self):
+        node = TextNode("This is text with a delimiter at the *end*", "text")
+        self.assertEqual(split_nodes_delimiter([node], "*", "italic"), [
+            TextNode("This is text with a delimiter at the ", "text"),
+            TextNode("end", "italic")
+        ])
+
+    def test_split_nodes_delimiter_non_text_node(self):
+        node = TextNode("This has wrong text type", "bold")
+        self.assertEqual(split_nodes_delimiter([node], "`", "code"), [
+		    TextNode("This has wrong text type", "bold")
+	    ])
+
+    def test_split_nodes_delimiter_not_in_text(self):
+        node = TextNode("This is text with a `code block` word", "text")
+        self.assertEqual(split_nodes_delimiter([node], "*", "italic"), [
+            TextNode("This is text with a `code block` word", "text")
+        ])
+
+    def test_split_nodes_delimiter_none(self):
+        node = TextNode("This is text with a `code block` word", "text")
+        with self.assertRaises(TypeError):
+            split_nodes_delimiter([node], None, None)
+
+    def test_split_nodes_delimiter_empty_string(self):
+        node = TextNode("", "text")
+        self.assertEqual(split_nodes_delimiter([node], "`", "code"), [TextNode("", "text")])
+
+    def test_consecutive_delimiters(self):
+        node = TextNode("This text has **consecutive** **delimiters**", "text")
+        self.assertEqual(split_nodes_delimiter([node], "**", "bold"), [
+            TextNode("This text has ", "text"),
+            TextNode("consecutive", "bold"),
+            TextNode(" ", "text"),
+            TextNode("delimiters", "bold")
+        ])
+
 class TestExtractMarkdown(unittest.TestCase):
     def test_extract_markdown_images(self):
         text = "This is text with a ![rick roll](https://i.imgur.com/aKaOqIh.gif) and ![obi wan](https://i.imgur.com/fJRm4Vk.jpeg)"
@@ -37,6 +124,14 @@ class TestExtractMarkdown(unittest.TestCase):
     def test_extract_markdown_links_no_matches(self):
         text = "This is text with a link to boot devhttps://www.boot.dev and to youtubehttps://www.youtube.com/@bootdotdev"
         self.assertEqual(extract_markdown_links(text), [])
+
+class TestSplitNodesImage(unittest.TestCase):
+    def test_split_nodes_image(self):
+        pass
+
+class TestSplitNodesLink(unittest.TestCase):
+    def test_split_nodes_link(self):
+        pass
 
 class TestMarkdownToBlocks(unittest.TestCase):
     def test_markdown_to_blocks(self):
