6290fd5d20eff05fce1f6c8d6c91e02235d23c52
Daniel <Daniel.halaas@gmail.com>
2024-10-14 14:56:53 +0200

    Moved text_to_textnodes from textnodes back to markdown

--- src/textnode.py
+++ src/textnode.py
@@ -1,5 +1,4 @@
 from helper_functions import *
-from markdown import split_nodes_delimiter, split_nodes_images, split_nodes_links
 
 text_type_text = "text"
 text_type_bold = "bold"
@@ -13,19 +12,6 @@ def text_node_to_html_node(text_node):
     if text_node.text_type not in type_to_node:
         raise Exception(f"invalid text type: {text_node.text_type}")
     return type_to_node[text_node.text_type](text_node)
-
-# converts raw markdown to TextNodes using helper split functions
-def text_to_textnodes(text: str):
-    if not text:
-        return []
-    node = TextNode(text, text_type_text)
-    result = []
-    result.extend(split_nodes_delimiter([node], "`", text_type_code))
-    result.extend(split_nodes_delimiter([node], "**", text_type_bold))
-    result.extend(split_nodes_delimiter([node], "*", text_type_italic))
-    #result.extend(split_nodes_image([node]))
-    #result.extend(split_nodes_link([node]))
-    return result
 
 class TextNode():
     def __init__(self, text, text_type, url=None):
