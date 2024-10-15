0dced56bf953e648e544d249d6cc049ac6675dfa
Daniel <Daniel.halaas@gmail.com>
2024-10-15 08:16:16 +0200

    Blank lines according to PEP 8

--- src/test_markdown.py
+++ src/test_markdown.py
@@ -111,7 +111,8 @@ class TestExtractMarkdown(unittest.TestCase):
     def test_extract_markdown_images_no_matches(self):
         text = "This is text with a [](https://i.imgur.com/aKaOqIh.gif) and !https://i.imgur.com/fJRm4Vk.jpeg"
         self.assertEqual(extract_markdown_images(text), [])
-        
+
+    
     def test_extract_markdown_links(self):
         text = "This is text with a link [to boot dev](https://www.boot.dev) and [to youtube](https://www.youtube.com/@bootdotdev)"
         self.assertEqual(extract_markdown_links(text), [
