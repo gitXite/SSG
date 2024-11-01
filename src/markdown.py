df466ced9e6446eec7ee9b65d32bc3c9b741cb7f
gitXite <daniel.halaas@gmail.com>
2024-11-01 11:16:19 +0100

    Fixed a bug in markdown_to_blocks

--- src/markdown.py
+++ src/markdown.py
@@ -124,10 +124,7 @@ def markdown_to_blocks(markdown):
     if not markdown:
         return []
     blocks = markdown.split("\n\n")
-    for block in blocks:
-        block = block.strip()
-        if not block:
-            blocks.remove(block)
+    blocks = [block.strip() for block in blocks if block.strip()]
     return blocks
 
 # checks the type of block
