fc36b46282cbf41e75b1d8f4d16928297e83aea9
Daniel <Daniel.halaas@gmail.com>
2024-11-06 16:50:23 +0100

    Update test_markdown.py

--- src/test_markdown.py
+++ src/test_markdown.py
@@ -609,7 +609,7 @@ class TestMarkdownToHTMLNode(unittest.TestCase):
     def test_more_generic_markdown(self):
         markdown = "### **This is a bold heading**\n\n*This is not a list\n*Because it gets treated as a paragraph\n\n1. This should also be a paragraph\n3. Because it has wrong numeration\n\n1. This however\n2. This is a sorted list\n3. Because it has the right syntax\n\n"
         self.assertEqual(markdown_to_html_node(markdown), HTMLNode("div", None, [
-            HTMLNode("h3", "This is a bold heading", [
+            HTMLNode("h3", "**This is a bold heading**", [
                 LeafNode("b", "This is a bold heading")
             ]),
             HTMLNode("p", "*This is not a list\n*Because it gets treated as a paragraph", [
